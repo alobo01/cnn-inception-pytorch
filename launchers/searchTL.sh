614b5fc17e1e7a399b1bd18289547065868bf71a
Antonio Lobo Desarrollo <alobosantos10@gmail.com>
2025-04-24 21:58:46 +0200

    inception everything

---
+++ launchers/searchTL.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+###
+
+#SBATCH --qos=train
+
+###
+
+#SBATCH --cpus-per-task=40
+
+#SBATCH --gres gpu:1
+
+#SBATCH --time=1-00:00:00
+
+###
+
+#SBATCH --job-name="searchTL"
+
+#SBATCH --chdir=.
+
+#SBATCH --output=searchTL_%j.out
+
+#SBATCH --error=searchTL_%j.err
+
+###
+
+module purgue
+module load  impi  intel  hdf5  mkl  python/3.12.1-gcc
+
+python train.py --config config/searchTL.yaml --tune
